8efac33c9b4e90695aac7e8c2a5d735dab39195b
Lê Phạm Ngọc Uyển <ngocuyenlepham@gmail.com>
2024-05-25 21:46:08 +0700

    feat: initial project

---
+++ demogithub/src/main/java/com/example/demogithub/DemogithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemogithubApplication.class, args);
+	}
+
+}
---
+++ demogithub/src/test/java/com/example/demogithub/DemogithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demogithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
